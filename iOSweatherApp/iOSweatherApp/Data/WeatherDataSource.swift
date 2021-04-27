bc0cf9a29d9ee83625be9fedeb8cec47d494d5e8
frozenxnow <frozenxnow@naver.com>
2021-04-28 00:07:23 +0900

    2021-04-28

---
+++ iOSweatherApp/iOSweatherApp/Data/WeatherDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherDataSource.swift
+//  iOSweatherApp
+//
+//  Created by 지원 on 2021/04/27.
+//
+
+import Foundation
---
+++ iOSweatherApp/iOSweatherApp/Model/CurrentWeather.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeather.swift
+//  iOSweatherApp
+//
+//  Created by 지원 on 2021/04/27.
+//
+
+import Foundation
